29c11cc16b4f893f7343bd08b64174842f06d43e
Ivan <isedletzki@scaleway.com>
2024-09-25 22:45:24 +0200

    Marketing cookie description

--- assets/cookieTypes.js
+++ assets/cookieTypes.js
@@ -4,5 +4,6 @@ const cookieTypes = {
   Analytics: 'Analytics Cookies, used to understand how users interact with the website.',
   Advertising: 'Advertising Cookies, used to provide personalized advertisements.',
   Security: 'Security Cookies, used to store and prevent fraudulent use of login credentials and protect user data from unauthorized parties.',
+  Marketing: 'Marketing Cookies, used to track and collect visitors\' behavior on the website and display targeted ads.',
   Unknown: 'Unknown Cookies, which are not in our database. This cookie is probably specific to this website and its purpose is not known.',
 }
